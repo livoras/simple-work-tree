49b072d6b0047e2e548849a3bb690d4fe002f997
DJH <livoras@163.com>
2026-04-13 16:35:30 -0700

    Fix current-worktree removal wrapper parsing

--- shell/swt.bash
+++ shell/swt.bash
@@ -1,13 +1,17 @@
 swt() {
   local target
+  local first_line
+  local remainder
   local switch_path
   local remove_path
 
   target="$(command swt "$@")" || return $?
-  if [[ "$target" == "__SWT_SWITCH_REMOVE__"$'\n'* ]]; then
-    switch_path="${target#*$'\n'}"
-    switch_path="${switch_path%%$'\n'*}"
-    remove_path="${target##*$'\n'}"
+  first_line="${target%%$'\n'*}"
+  if [[ "$first_line" == "__SWT_SWITCH_REMOVE__" ]]; then
+    remainder="${target#*$'\n'}"
+    switch_path="${remainder%%$'\n'*}"
+    remove_path="${remainder#*$'\n'}"
+    [[ -n "$switch_path" && -n "$remove_path" ]] || return 1
 
     cd "$switch_path" || return
     git -C "$switch_path" worktree remove "$remove_path"
--- shell/swt.zsh
+++ shell/swt.zsh
@@ -1,15 +1,16 @@
 swt() {
   emulate -L zsh
   local target
-  local rest
+  local -a lines
   local switch_path
   local remove_path
 
   target="$(command swt "$@")" || return $?
-  if [[ "$target" == "__SWT_SWITCH_REMOVE__"$'\n'* ]]; then
-    rest="${target#*$'\n'}"
-    switch_path="${rest%%$'\n'*}"
-    remove_path="${rest#*$'\n'}"
+  lines=("${(@f)target}")
+  if [[ "${lines[1]:-}" == "__SWT_SWITCH_REMOVE__" ]]; then
+    switch_path="${lines[2]:-}"
+    remove_path="${lines[3]:-}"
+    [[ -n "$switch_path" && -n "$remove_path" ]] || return 1
 
     cd "$switch_path" || return
     git -C "$switch_path" worktree remove "$remove_path"
